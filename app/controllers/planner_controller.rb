307ad0a9032213d7dd970357f4dfcfb95e1616fc
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-11-23 22:18:13 +0000

    style no-results and clean up planner code

--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -2,7 +2,10 @@ class PlannerController < ApplicationController
     before_action :authenticate_user!
 
     def index
-        @upcoming = Task.claimed_by(current_user).incomplete.order(urgent: :desc)
+        upcoming = Task.claimed_by(current_user).incomplete.order(urgent: :desc)
+
+        @next = upcoming.first
+        @upcoming = upcoming.drop(1)
         @recently_completed = Task.claimed_by(current_user).recently_completed
     end
 end
