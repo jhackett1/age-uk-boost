35884039d19769410085673a6946fd073f950500
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-11-23 22:40:27 +0000

    vh fix

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,7 +1,12 @@
 import "core-js/stable"
 import "regenerator-runtime/runtime"
 
+import vhCheck from 'vh-check'
+
 import "leaflet/dist/leaflet"
 
 import "./_maps"
 import "./_autosubmit-filters"
+
+
+vhCheck()
